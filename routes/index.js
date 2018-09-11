606b4e81e61700c119293fb1a79fa622a21dcc20
Ashell Fernando <ashellz2010@gmail.com>
2018-09-11 11:28:48 -0400

    removed async for now

--- routes/index.js
+++ routes/index.js
@@ -7,28 +7,32 @@ const cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'MovDow - movie search app.' });
+	res.render('index', { title: 'MovDow - movie search app', sub_title: 'movie download', displayResults: '' });
 });
-/* GET search page.  */
+/* /* GET search page.  
 router.get('/search', (req, res, next) => {
 	res.render('search', { title: 'Search Page', search_results: `${id}` });
-});
+}); */
 
 router.post('/', (req, res, err) => {
+	/* res has the imdb movie id */
 	var movie_name = req.body.id_field;
-	nameToImdb({ name: movie_name }, async (err, res, type) => {
-		const movie_id = await res;
-		if (res != 0) {
-			/* This is for IMDB */
+	nameToImdb({ name: movie_name }, (err, res, type) => {
+		const movie_id = res;
+		if (!err) {
+			/* This is for IMDB, searching the movie id with the name of the movie */
 			new request(`https://imdb.com/title/${movie_id}/`, (error, response, html) => {
 				if (!error && response.statusCode == 200) {
 					const imdbHTML = cheerio.load(html);
 
 					const imdbTitleWrapper = imdbHTML('.title_wrapper');
 
-					const title = imdbTitleWrapper.find('h1').text();
+					const title = imdbTitleWrapper
+						.find('h1')
+						.text()
+						.replace(/\s\s+g/, '');
 
-					console.log(`${title} ${movie_id}`);
+					console.log(title, movie_id);
 				}
 			});
 			/* This is for rarbg */
@@ -42,6 +46,10 @@ router.post('/', (req, res, err) => {
 			);
 		}
 	});
-	// console.log(movie_id);
+	res.render('index', {
+		displayResults: movie_name,
+		title: 'MovDow - a movie torrent search app',
+		sub_title: movie_name,
+	});
 });
 module.exports = router;
